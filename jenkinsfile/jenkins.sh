eddcb7060e3606fc0de7fca88c57384ed2655661
root <root@ip-172-31-95-241.ec2.internal>
2024-04-17 17:12:16 +0000

    script modified

--- jenkinsfile/jenkins.sh
+++ jenkinsfile/jenkins.sh
@@ -4,11 +4,7 @@ pipeline{
         stage('Build'){
             steps{
                 sh '''
-<<<<<<< HEAD
-                echo Devops Task-1 and  Task-2 completed 
-=======
-                echo Devops Task-1 completed
->>>>>>> ea9606da3d8951cb52c49cc418a3ef130c91ffef
+                echo Devops Task-1 and  Task-2 completed
                 '''
             }
         }
